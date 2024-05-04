8a9b9cc4549fc28f54963068355dd7e26019a54a
Guilherme Silva <guilherme.tr.silva@gmail.com>
2024-05-04 18:00:22 -0300

    fix: update echo to echo v4

--- server/echoServer.go
+++ server/echoServer.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/br4tech/go-with-gemini/config"
 	"github.com/br4tech/go-with-gemini/internal/core/port"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/gorm"
 )
 
@@ -16,19 +16,23 @@ type echoServer struct {
 	cfg            *config.Config
 	opinionHandler port.IOpinionHandler
 	productHandler port.IProductHandler
-	summyHandler   port.ISummaryHandler
+	summaryHandler port.ISummaryHandler
 }
 
-func NewEchoServer(cfg *config.Config, db *gorm.DB,
+func NewEchoServer(
+	cfg *config.Config,
+	db *gorm.DB,
 	opinionHandler port.IOpinionHandler,
 	productHandler port.IProductHandler,
-	summyHandler port.ISummaryHandler,
+	summaryHandler port.ISummaryHandler,
 ) Server {
 	return &echoServer{
 		app:            echo.New(),
 		db:             db,
 		cfg:            cfg,
 		opinionHandler: opinionHandler,
+		productHandler: productHandler,
+		summaryHandler: summaryHandler,
 	}
 }
 
@@ -37,14 +41,15 @@ func (s *echoServer) Start() {
 		Format: "${time_rfc3339} ${status} ${method} ${host}${path} ${latency_human}\n",
 	}))
 
-	s.app.POST("/product", s.productHandler.CreateOpinion)
+	s.app.POST("/product", s.productHandler.CreateProduct)
 	s.app.GET("/product", s.productHandler.Find)
 
 	s.app.POST("/opinion", s.opinionHandler.CreateOpinion)
+	s.app.GET("/{product_id}/opinions", s.opinionHandler.FindByProductId)
 	s.app.GET("/opinion", s.opinionHandler.Find)
 
-	s.app.POST("/summary", s.summyHandler.CreateOpinion)
-	s.app.GET("/summary", s.summyHandler.Find)
+	s.app.POST("/summary", s.summaryHandler.CreateSummary)
+	s.app.GET("/summary", s.summaryHandler.Find)
 
 	serverUrl := fmt.Sprintf(":%d", s.cfg.App.Port)
 	s.app.Logger.Fatal(s.app.Start(serverUrl))
